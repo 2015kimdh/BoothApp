81e40f7a14050b096daaeed951b73286b2961720
2015kimdh <43131724+2015kimdh@users.noreply.github.com>
2025-04-07 23:55:57 +0900

    판매 기능 시작

--- Assets/BoothApp/Presentation/BoothDetail/PurchaseItem/PurchaseItemSelectButton.cs
+++ Assets/BoothApp/Presentation/BoothDetail/PurchaseItem/PurchaseItemSelectButton.cs
@@ -1,9 +1,14 @@
+using Doozy.Runtime.UIManager.Components;
 using UnityEngine;
 
 namespace BoothApp.Presentation.BoothDetail.PurchaseItem
 {
     public class PurchaseItemSelectButton : MonoBehaviour
     {
-        
+        #region Serialize Field
+
+        [SerializeField] private UIButton button;
+
+        #endregion
     }
 }
